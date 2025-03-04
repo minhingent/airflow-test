b9f5f885a949b9a931e9a85546c8bba991407a53
Nguyễn Hữu Minh <huuminh.nguyen@quantum.com>
2025-03-04 07:54:37 +0000

    online example

--- dags/simple_k8s_hello_world.py
+++ dags/simple_k8s_hello_world.py
@@ -7,6 +7,7 @@ from collections import namedtuple
 from kubernetes.client import models as k8s
 import logging
 import time
+from datetime import datetime
 import uuid
 
 
@@ -19,18 +20,45 @@ default_args = {
 
 LOGGER = logging.getLogger("airflow.task")  # Get Airflow logger
 
-@dag(
-    schedule=None,
-    start_date=days_ago(1),
-    description='KubernetesExecutor with Taskflow API',
-    schedule_interval=None,
-    default_args=default_args,
-)
-def simple_k8s_hello_world():
-    @task.kubernetes(image="apache/airflow:2.9.3", namespace="airflow")
-    def hello_world():
+# @dag(
+#     schedule=None,
+#     start_date=days_ago(1),
+#     description='KubernetesExecutor with Taskflow API',
+#     schedule_interval=None,
+#     default_args=default_args,
+# )
+# def simple_k8s_hello_world():
+#     @task.kubernetes(image="apache/airflow:2.9.3", namespace="airflow")
+#     def hello_world():
+#         print('hello k8s')
+
+#     hello_world()
+
+# simple_k8s_hello_world()
+
+
+new_config ={ "pod_override": k8s.V1Pod(
+                spec=k8s.V1PodSpec(
+                    containers=[
+                        k8s.V1Container(
+                            name="base",
+                            )
+                        ]
+                    )
+                )
+            }
+
+default_args = {
+    'start_date': datetime(2021, 1, 1)
+}
+
+@dag('simple_k8s_hello_world', schedule_interval=None, default_args=default_args, catchup=False)
+def taskflow():
+
+    @task(executor_config=new_config)
+    def get_testing_increase():
         print('hello k8s')
 
-    hello_world()
+    get_testing_increase()
 
-simple_k8s_hello_world()
+dag = taskflow()
